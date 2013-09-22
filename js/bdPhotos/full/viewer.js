6157079481ae4f415f82d2659ea6f8a4a11cebdc
Dao Hoang Son <daohoangson@gmail.com>
2013-09-23 00:57:57 +0700

    added ajax overlay for photos in list

--- js/bdPhotos/full/viewer.js
+++ js/bdPhotos/full/viewer.js
@@ -5,9 +5,14 @@ function($, window, document, _undefined)
 
 	$(window).bind('popstate', function(e)
 	{
-		var state = (e.originalEvent.state ? e.originalEvent.state : $bdPhotos_NavigationLink_firstState);
+		var state = e.originalEvent.state;
 
-		if (state)
+		if (!state || !state._owner || state._owner != 'bdPhotos_NavigationLink_updateHtml')
+		{
+			state = $bdPhotos_NavigationLink_firstState;
+		}
+
+		if (state && state._owner && state._owner == 'bdPhotos_NavigationLink_updateHtml')
 		{
 			var $photo = $('.bdPhotos_Photo');
 
@@ -55,6 +60,7 @@ function($, window, document, _undefined)
 			{
 				$bdPhotos_NavigationLink_firstState =
 				{
+					'_owner': 'bdPhotos_NavigationLink_updateHtml',
 					'title': document.title,
 					'href': window.location.href,
 					'photoHtml': oldPhotoHtml,
@@ -64,6 +70,7 @@ function($, window, document, _undefined)
 
 			window.history.pushState(
 			{
+				'_owner': 'bdPhotos_NavigationLink_updateHtml',
 				'title': title,
 				'href': href,
 				'photoHtml': photoHtml,
@@ -92,6 +99,18 @@ function($, window, document, _undefined)
 
 		click: function(e)
 		{
+			// abort if the event has a modifier key
+			if (e.ctrlKey || e.shiftKey || e.altKey || e.metaKey)
+			{
+				return true;
+			}
+
+			// abort if the event is a middle or right-button click
+			if (e.which > 1)
+			{
+				return true;
+			}
+
 			if (this.xhr)
 			{
 				this.xhr.abort();
@@ -124,6 +143,140 @@ function($, window, document, _undefined)
 
 	// *********************************************************************
 
+	XenForo.bdPhotos_OverlayTrigger = function($trigger)
+	{
+		this.__construct($trigger);
+	};
+	XenForo.bdPhotos_OverlayTrigger.prototype =
+	{
+		__construct: function($trigger)
+		{
+			this.$trigger = $trigger.click($.context(this, 'show'));
+			this.options =
+			{
+				className: 'primaryContent bdPhotos_Overlay',
+				closeOnResize: true,
+				mask:
+				{
+					color: 'black'
+				}
+			};
+
+			this.$trigger.bind(
+			{
+				onLoad: $.context(this, 'overlayLoad'),
+				onClose: $.context(this, 'overlayClose'),
+			});
+
+			this.historyCount = window.history.length;
+			this.documentTitle = document.title;
+		},
+
+		overlayCallback: function()
+		{
+			var api = this.$trigger.data('overlay');
+			var $overlay = api.getOverlay();
+
+			var $photoWrapperWrapper = $overlay.find('.bdPhotos_PhotoWrapperWrapper');
+			var $photo = $overlay.find('.bdPhotos_PhotoWrapper img');
+
+			var windowWidth = $(window).width();
+			var photoWidth = Math.floor(windowWidth / 5 * 3);
+			var photoHeight = Math.floor($(window).height() / 5 * 4);
+
+			var css = '.bdPhotos_Overlay .bdPhotos_Photo .bdPhotos_PhotoWrapperWrapper table { width: ' + photoWidth + 'px; }';
+			css += '.bdPhotos_Overlay .bdPhotos_PhotoWrapper { height: ' + (photoHeight + 5) + 'px; }';
+			css += '.bdPhotos_Overlay .bdPhotos_PhotoWrapper img { max-width: ' + photoWidth + 'px; max-height: ' + photoHeight + 'px; }';
+			css += '.bdPhotos_Overlay .bdPhotos_PhotoComments { margin-left: ' + (photoWidth + 5) + 'px; }';
+
+			var $style = $('#bdPhotos_OverlayTrigger_Css');
+			if ($style.length > 0)
+			{
+				$style.empty().remove();
+			}
+			$('<style id="bdPhotos_OverlayTrigger_Css">' + css + '</style>').appendTo('head');
+		},
+
+		overlayClose: function(e)
+		{
+			if (window.history.pushState)
+			{
+				var count = window.history.length;
+
+				window.history.go(this.historyCount - count);
+				document.title = this.documentTitle;
+			}
+
+			$(window).unbind('popstate', $.context(this, 'windowPopState'));
+		},
+
+		overlayLoad: function(e)
+		{
+			if (window.history.pushState)
+			{
+				$bdPhotos_NavigationLink_firstState = null;
+
+				window.history.pushState(
+				{
+					'_owner': 'bdPhotos_OverlayTrigger',
+				}, '', this.$trigger.attr('href'));
+
+				document.title = this.$trigger.data('overlay').getConf().title;
+			}
+
+			$(window).bind('popstate', $.context(this, 'windowPopState'));
+		},
+
+		windowPopState: function(e)
+		{
+			var state = e.originalEvent.state;
+
+			if (!state)
+			{
+				this.$trigger.data('overlay').close();
+			}
+		},
+
+		show: function(e)
+		{
+			if (this.$trigger.hasClass('NoOverlay'))
+			{
+				return true;
+			}
+
+			// abort if the event has a modifier key
+			if (e.ctrlKey || e.shiftKey || e.altKey || e.metaKey)
+			{
+				return true;
+			}
+
+			// abort if the event is a middle or right-button click
+			if (e.which > 1)
+			{
+				return true;
+			}
+
+			// abort if the window is too small
+			if ($(window).width() < 500)
+			{
+				return true;
+			}
+
+			e.preventDefault();
+
+			options = $.extend(this.options, this.$trigger.data('overlayoptions'));
+
+			this.OverlayLoader = new XenForo.OverlayLoader(this.$trigger, false, options);
+			this.OverlayLoader.load($.context(this, 'overlayCallback'));
+
+			return true;
+
+		}
+	};
+
+	// *********************************************************************
+
 	XenForo.register('a.bdPhotos_NavigationLink', 'XenForo.bdPhotos_NavigationLink');
+	XenForo.register('a.bdPhotos_OverlayTrigger', 'XenForo.bdPhotos_OverlayTrigger');
 
 }(jQuery, this, document);
